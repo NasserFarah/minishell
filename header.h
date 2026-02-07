cce7a10d15a2a9aaed1d75387d476752e0bd4dc4
Farah <farah.s.nasser@gmail.com>
2026-02-07 23:38:57 +0200

    Add file

---
+++ header.h
@@ -0,0 +1,15 @@
+// fnasser
+// abdunass
+
+#ifndef HEADER_H
+# define HEADER_H
+
+# include <unistd.h>
+# include <stdlib.h>
+# include <stdio.h>
+# include <string.h>
+# include "libft.h"
+# include "ft_printf.h"
+
+
+#endif
